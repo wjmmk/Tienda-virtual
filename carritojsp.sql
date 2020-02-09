6f5a51ef5060b1092d364a731325a8296f3e9afe
William J. Mosquera Machado <wjmmk3@gmail.com>
2020-02-09 15:54:22 -0500

    Agregando BD.
    Se agrego la base de datos de pruebas.

---
+++ carritojsp.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-02-2020 a las 21:46:47
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `carritojsp`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selectProducto` (IN `_id_producto` INT)  BEGIN
+		SELECT * FROM productos WHERE id_producto = _id_producto;
+	END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selectProductos` ()  BEGIN
+		SELECT * FROM productos;
+	END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `img_producto` varchar(255) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `precio` float NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id_producto`, `nombre`, `img_producto`, `id_categoria`, `precio`, `stock`) VALUES
+(1, 'Vaso team Cap. America', 'images/home/vaso.jpg', 2, 39.99, 10),
+(2, 'Pantera Negra', 'images/home/panther.jpg', 2, 19.99, 20),
+(3, 'Thanos Endgame', 'images/home/Thanos.jpg', 2, 29.99, 20),
+(4, 'Iron Man - The Best', 'images/home/ironman.jpg', 2, 13.99, 15),
+(5, 'vaso ~ Smood', 'images/home/smood.jpg', 2, 15.99, 18),
+(6, 'Gorra - Avengers', 'images/home/gorra.jpg', 2, 29.99, 16);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
